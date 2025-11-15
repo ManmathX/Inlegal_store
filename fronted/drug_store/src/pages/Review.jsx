8ee005b451afa53bf99c6b91db21f4472292ef06
ighackerbot <jainanuj2005@gmail.com>
2025-11-15 18:19:23 +0530

    intro added
    ertyukl;

---
+++ fronted/drug_store/src/pages/Review.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Review() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Reviews</h1>
+      <p>Customer reviews will appear here.</p>
+    </div>
+  )
+}
+
+export default Review
